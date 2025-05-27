0f2860c42c60766033998edc84a8f19ab38d6311
ege2734 <110061031+ege2734@users.noreply.github.com>
2025-05-27 16:21:07 -0400

    working version with google

--- api/index.py
+++ api/index.py
@@ -3,156 +3,62 @@ from typing import List
 
 from fastapi import FastAPI, Query
 from fastapi.responses import StreamingResponse
-from openai import OpenAI
-from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
+from google import genai
+from google.genai.types import UsageMetadata
 from pydantic import BaseModel
 
-from .utils.prompt import ClientMessage, convert_to_openai_messages
+from .utils.prompt import ClientMessage
 from .utils.settings import get_setting
-from .utils.tools import get_current_weather
 
 app = FastAPI()
 
-client = OpenAI(api_key=get_setting("OPENAI_API_KEY"))
+model = "gemini-2.0-flash-live-001"
+config = {"response_modalities": ["TEXT"]}
+
+client = genai.Client(api_key=get_setting("GEMINI_API_KEY"))
 
 
 class Request(BaseModel):
     messages: List[ClientMessage]
 
 
-available_tools = {
-    "get_current_weather": get_current_weather,
-}
+def create_text_response(text: str):
+    return f"0:{json.dumps(text)}\n".encode("utf-8")
 
 
-def do_stream(messages: List[ChatCompletionMessageParam]):
-    stream = client.chat.completions.create(
-        messages=messages,
-        model="gpt-4o",
-        stream=True,
-        tools=[
-            {
-                "type": "function",
-                "function": {
-                    "name": "get_current_weather",
-                    "description": "Get the current weather at a location",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "latitude": {
-                                "type": "number",
-                                "description": "The latitude of the location",
-                            },
-                            "longitude": {
-                                "type": "number",
-                                "description": "The longitude of the location",
-                            },
-                        },
-                        "required": ["latitude", "longitude"],
-                    },
-                },
-            }
-        ],
-    )
-
-    return stream
+def create_end_response(finish_reason: str, usage_metadata: UsageMetadata):
+    return_dict = {
+        "finishReason": finish_reason,
+        "usage": {
+            "promptTokens": usage_metadata.prompt_token_count,
+            "completionTokens": usage_metadata.response_token_count,
+        },
+        "isContinued": False,
+    }
+    return f"d:{json.dumps(return_dict)}\n".encode("utf-8")
 
 
-def stream_text(messages: List[ChatCompletionMessageParam], protocol: str = "data"):
-    draft_tool_calls = []
-    draft_tool_calls_index = -1
+async def do_stream(messages: List[ClientMessage]):
+    async with client.aio.live.connect(model=model, config=config) as session:
+        await session.send_client_content(
+            turns={"role": "user", "parts": [{"text": messages[-1].content}]},
+            turn_complete=True,
+        )
+        async for response in session.receive():
+            print(response)
+            if response.text is not None:
+                yield create_text_response(response.text)
 
-    stream = client.chat.completions.create(
-        messages=messages,
-        model="gpt-4o",
-        stream=True,
-        tools=[
-            {
-                "type": "function",
-                "function": {
-                    "name": "get_current_weather",
-                    "description": "Get the current weather at a location",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "latitude": {
-                                "type": "number",
-                                "description": "The latitude of the location",
-                            },
-                            "longitude": {
-                                "type": "number",
-                                "description": "The longitude of the location",
-                            },
-                        },
-                        "required": ["latitude", "longitude"],
-                    },
-                },
-            }
-        ],
-    )
-
-    for chunk in stream:
-        for choice in chunk.choices:
-            if choice.finish_reason == "stop":
-                continue
-
-            elif choice.finish_reason == "tool_calls":
-                for tool_call in draft_tool_calls:
-                    yield '9:{{"toolCallId":"{id}","toolName":"{name}","args":{args}}}\n'.format(
-                        id=tool_call["id"],
-                        name=tool_call["name"],
-                        args=tool_call["arguments"],
-                    )
-
-                for tool_call in draft_tool_calls:
-                    tool_result = available_tools[tool_call["name"]](
-                        **json.loads(tool_call["arguments"])
-                    )
-
-                    yield 'a:{{"toolCallId":"{id}","toolName":"{name}","args":{args},"result":{result}}}\n'.format(
-                        id=tool_call["id"],
-                        name=tool_call["name"],
-                        args=tool_call["arguments"],
-                        result=json.dumps(tool_result),
-                    )
-
-            elif choice.delta.tool_calls:
-                for tool_call in choice.delta.tool_calls:
-                    id = tool_call.id
-                    name = tool_call.function.name
-                    arguments = tool_call.function.arguments
-
-                    if id is not None:
-                        draft_tool_calls_index += 1
-                        draft_tool_calls.append(
-                            {"id": id, "name": name, "arguments": ""}
-                        )
-
-                    else:
-                        draft_tool_calls[draft_tool_calls_index][
-                            "arguments"
-                        ] += arguments
-
-            else:
-                yield "0:{text}\n".format(text=json.dumps(choice.delta.content))
-
-        if chunk.choices == []:
-            usage = chunk.usage
-            prompt_tokens = usage.prompt_tokens
-            completion_tokens = usage.completion_tokens
-
-            yield 'e:{{"finishReason":"{reason}","usage":{{"promptTokens":{prompt},"completionTokens":{completion}}},"isContinued":false}}\n'.format(
-                reason="tool-calls" if len(draft_tool_calls) > 0 else "stop",
-                prompt=prompt_tokens,
-                completion=completion_tokens,
-            )
+            if response.server_content.turn_complete:
+                yield create_end_response(
+                    finish_reason="stop",
+                    usage_metadata=response.usage_metadata,
+                )
 
 
 @app.post("/api/chat")
 async def handle_chat_data(request: Request, protocol: str = Query("data")):
-    messages = request.messages
-    openai_messages = convert_to_openai_messages(messages)
+    response = StreamingResponse(do_stream(request.messages))
 
-    response = StreamingResponse(stream_text(openai_messages, protocol))
     response.headers["x-vercel-ai-data-stream"] = "v1"
     return response
